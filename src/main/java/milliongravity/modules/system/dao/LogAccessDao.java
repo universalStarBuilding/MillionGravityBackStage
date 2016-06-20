3f1e4dab42d07464697a6fae7755e2da6365accf
lehuohuo <lehuohuo016@gmail.com>
2016-06-20 16:26:43 +0800

    sds

--- src/main/java/milliongravity/modules/system/dao/LogAccessDao.java
+++ src/main/java/milliongravity/modules/system/dao/LogAccessDao.java
@@ -1,8 +1,9 @@
 package milliongravity.modules.system.dao;
 
 import milliongravity.common.persistence.CrudDao;
+import milliongravity.common.persistence.annotation.MyBatisDao;
 import milliongravity.modules.system.entity.LogAccess;
-
+@MyBatisDao
 public interface LogAccessDao extends CrudDao<LogAccess>{
 
 }
